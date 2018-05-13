016be60dfe1f492362b5189f329074a80c3d350e
赵沙沙 <2928501731@qq.com>
2018-05-13 21:27:07 +0800

    fifth homework

---
+++ zhaoshasha/05/ToDoList/js/main.js
@@ -0,0 +1,58 @@
+var header = $('header')
+    .css({
+    	height: '50px',
+    	background: 'blue',
+    })
+
+var label = $('label')
+    .css({
+    	float: 'left',
+    	width: '100px',
+    	'line-height': '50px',
+    	color: '#DDD',
+    	'font-size': '24px',
+    	cursor: 'pointer',  	
+    })
+    
+var input = $('header input')
+    .css({
+    	float: 'right',
+    	width: '60%',
+    	height: '24px',
+    	'margin-top': '12px',
+    	'text-indent': '10px',
+    	'border-radius': '5px',
+    	'box-shadow': '0 1px 0 rgba(255, 255, 255, 0.24), 0 1px 6px rgba(0, 0, 0, 0.45) inset',
+    	border:'none',  	
+    })
+
+$('h2').css('position','relative');
+
+var span = $('span')
+    .css({
+    	position: 'absolute',
+    	top: '2px',
+    	right: '5px',
+    	display: 'inline-block',
+    	padding: '0 5px',
+    	height: '20px',
+    	'border-radius': '20px',
+    	background: '#E6E6FA',
+    	'line-height': '22px',
+    	'text-align': 'center',
+    	color: '#666',
+    	'font-size': '14px',
+    })
+var footer = $('footer')
+    .css({
+    	color: '#666',
+    	'font-size': '14px',
+    	'text-align': 'center',
+    })
+    
+var a = $('footer a')
+    .css({
+    	color: 'blue',
+    	'text-decoration': 'none', 	
+    })
+
